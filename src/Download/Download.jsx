4944d76798cd004bdb4ce49a8eb61bab261bedd9
aolguin <full_stack_at@talleres.cl>
2025-06-04 11:37:02 -0400

    Fix en apks, espero...

--- src/Download/Download.jsx
+++ src/Download/Download.jsx
@@ -36,7 +36,7 @@ const Download = () => {
         </Toolbar>
       </AppBar>
 
-      <Box sx={{ position: "relative", width: "100%", height: "900px", backgroundImage: `url(${BGTop})`, backgroundSize: "cover", backgroundPosition: "center", display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+      <Box sx={{ position: "relative", width: "100%", height: "1500px", backgroundImage: `url(${BGTop})`, backgroundSize: "cover", backgroundPosition: "center", display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
         <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", backgroundColor: "rgba(255, 0, 0, 0.4)" }} />
 
         {/* Componente de Pago */}
